ddfe04864c344bc6e1374f2e312641b0878eaf32
larry eliemenye <denachural@gmail.com>
2017-11-18 22:59:39 +0000

    Added some logic to create scheme from main contract.

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -21,12 +21,13 @@ contract Scheme {
     // leave the scheme
     function leave() public;
     
-    //pays a member based on implemented scheme rules
-    function pay() onlyMember public;
+    // pays a member based on implemented scheme rules
+    function pay() onlyMember public payable;
     
     // deposit/contribute funds to the scheme
     function contribute () public onlyMember payable;
     
+    // checks if an address is a memeber of the scheme;
     function isMemberAddress(address toCheck) private view returns (bool);
 }
 
@@ -42,7 +43,7 @@ contract RotationScheme is Scheme {
 
     uint contributionAmount;
     uint contributionCount = 0;
-    uint currentContributionRound = 1;
+    uint currentContributionRound = 0;
     uint maxMemberSize = 15;
 
     address initiator;
@@ -58,7 +59,7 @@ contract RotationScheme is Scheme {
     /**
         this contract is automatically created from the main contract
      */
-    function RotationScheme (address _initiator, uint _maxMemberSize, uint _contributionAmount) {
+    function RotationScheme (uint _maxMemberSize, uint _contributionAmount) public {
         // set maximum size on creation
         maxMemberSize = _maxMemberSize;
 
@@ -69,7 +70,11 @@ contract RotationScheme is Scheme {
         contributionAmount = _contributionAmount;
 
         // set foreperson/initiator as owner of contract
-        initiator = _initiator;
+        initiator = msg.sender;
+    }
+    
+    function getInitiator () public view returns (address) {
+        return initiator;
     }
     
     // for functions required only on recruitment phase
@@ -100,32 +105,65 @@ contract RotationScheme is Scheme {
     // implement joining this scheme. One can only join when contract is on recruitement pphace
     function join() public onlyOnRecruit isValidContribution payable {
         balances[msg.sender] += msg.value;
-        Contribution memory contribution = Contribution(++contributionCount, msg.sender, now);
+        Contribution memory contribution = Contribution(++contributionCount, msg.sender, block.timestamp);
         contributions.push(contribution);
     }
 
     // address to send previously deposited funds to if any
-    function leave (address leaverAddress) public onlyOnRecruit () {
-        if(currennt)
+    function leave () public onlyOnRecruit () {
+        
+    }
+    
+    function contribute() public payable {
+        
+    }
+    
+    function isMemberAddress(address toCheck) private view returns (bool) {
+        
+    }
+    
+    function pay() public payable {
+        
     }
 }
 
 contract Main {
     // all contracts
-    Scheme[] contracts;
+    uint schemeCounter = 0;
+    mapping (uint => address) schemes;
     
     // supported schemes
     enum SchemeTypes { ROSCA, ASCA, ASCA_LOAN }
     
+    event LogNewContractCreated(address initiator, uint contractId);
+    
+    modifier isContractInitiator(uint schemeId) {
+        address scheme = schemes[schemeId];
+        if (msg.sender != Scheme(scheme).getInitiator()) {
+            revert();
+        }
+        _;
+    }
+    
     // creates a new scheme
-    function createNewScheme (uint schemeType) private returns (address) {
+    function createNewScheme (uint schemeType, uint memberSize, uint amountPerContribution) private returns (Scheme) {
         if (schemeType == uint(SchemeTypes.ROSCA)) {
-            return new RotationScheme(msg.value);
+            return new RotationScheme(memberSize, amountPerContribution);
         }
     }
     
+    // 
+    function startScheme(uint schemeType, uint size, uint contributionAmount) public {
+        uint schemeId = ++schemeCounter;
+        schemes[++schemeCounter] = createNewScheme(schemeType, size, contributionAmount);
+        LogNewContractCreated(msg.sender, schemeId);
+    }
+    
     // terminate contract
-    function destroyScheme (address contractToDestroy) public {
-        
+    // contracts can only be terminated when they in the RECRUITING 
+    // phase of which funds will be returned to existing participants
+    function destroyScheme(uint _schemeId) public isContractInitiator(uint schemeId) {
+        address scheme = schemes[_schemeId];
+        address initiator = Scheme(scheme).getInitiator();
     }
 }
